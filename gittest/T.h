853ecf552ba6c06e01d6fdd3fc4474d12c7744a0
chenshengkehua <chenshengkehua@chenshengkehuadeMacBook-Pro.local>
2014-02-13 22:19:26 +0800

    h

---
+++ gittest/T.h
@@ -0,0 +1,13 @@
+//
+//  T.h
+//  gittest
+//
+//  Created by chenshengkehua on 14-2-13.
+//  Copyright (c) 2014年 xiaobi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface T : UIWebView
+
+@end
---
+++ gittest/T.m
@@ -0,0 +1,31 @@
+//
+//  T.m
+//  gittest
+//
+//  Created by chenshengkehua on 14-2-13.
+//  Copyright (c) 2014年 xiaobi. All rights reserved.
+//
+
+#import "T.h"
+
+@implementation T
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
